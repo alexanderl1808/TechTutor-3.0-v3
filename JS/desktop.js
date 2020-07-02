0fcb124462d731a228cffea6d4f057e98ceca789
Milanrdnvc <61325920+Milanrdnvc@users.noreply.github.com>
2020-07-02 15:52:15 +0200

    Add files via upload

--- JS/desktop.js
+++ JS/desktop.js
@@ -1,28 +1,56 @@
-const slidesContainer = document.querySelector('.desktop-section__img-container');
-const slides = document.querySelectorAll('.desktop-section__img-price-container');
-const arrowLeft = document.querySelector('.fa-arrow-left');
-const arrowRight = document.querySelector('.fa-arrow-right');
+const slidesContainer = document.querySelectorAll('.desktop-section__img-container');
+const slidesFirst = document.querySelectorAll('.slides-first');
+const slidesSecond = document.querySelectorAll('.slides-second');
+const slidesThird = document.querySelectorAll('.slides-third');
+const arrows = document.querySelectorAll('.arrow');
+const arrowLeft = document.querySelectorAll('.fa-arrow-left');
+const arrowRight = document.querySelectorAll('.fa-arrow-right');
+const btn = document.querySelectorAll('.desktop-section__btn');
+const para = document.querySelectorAll('.desktop-section__para-container');
 
-slides.forEach((slide, index) => {
-    slide.style.left = `${100 * index}%`;
-});
+const events = ['load', 'resize'];
+for(let event of events){
+    window.addEventListener(event, () => {
+        slidesFirst.forEach((slide, index) => {
+            slide.style.left = `${100 * index}%`;
+        });
+        slidesSecond.forEach((slide, index) => {
+            slide.style.left = `${100 * index}%`;
+        });
+        slidesThird.forEach((slide, index) => {
+            slide.style.left = `${100 * index}%`;
+        });
+    });
+}
 
 function moveToSlide(slidesContainer, currentSlide, targetSlide){
     slidesContainer.style.transform = `translateX(-${targetSlide.style.left})`;
     currentSlide.classList.remove('current-slide');
     targetSlide.classList.add('current-slide');
-
 }
 
-arrowRight.addEventListener('click', () => {
-    const currentSlide = slidesContainer.querySelector('.current-slide');
-    const nextSlide = currentSlide.nextElementSibling;
-    moveToSlide(slidesContainer, currentSlide, nextSlide);
+arrowRight.forEach((arrow, index) => {
+    arrow.addEventListener('click', () => {
+        const currentSlide = slidesContainer[index].querySelectorAll('.current-slide')[0];
+        const nextSlide = currentSlide.nextElementSibling;
+        moveToSlide(slidesContainer[index], currentSlide, nextSlide);
+    });
 });
 
-arrowLeft.addEventListener('click', () => {
-    const currentSlide = slidesContainer.querySelector('.current-slide');
-    const previousSlide = currentSlide.previousElementSibling;
-    moveToSlide(slidesContainer, currentSlide, previousSlide);
+arrowLeft.forEach((arrow, index) => {
+    arrow.addEventListener('click', () => {
+        const currentSlide = slidesContainer[index].querySelectorAll('.current-slide')[0];
+        const previousSlide = currentSlide.previousElementSibling;
+        moveToSlide(slidesContainer[index], currentSlide, previousSlide);
+    });
 });
+
+btn.forEach((btn, index) => {
+    btn.addEventListener('click', () => {
+        para[index].classList.toggle('para-container--transform');
+        slidesContainer[index].classList.toggle('img-container--transform');
+        arrows[index].classList.toggle('img-container--transform');
+    });
+});
+
 
