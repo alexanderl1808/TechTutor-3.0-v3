50fcc969a567e5a47b0ea1123425881bb7a32619
Milanrdnvc <61325920+Milanrdnvc@users.noreply.github.com>
2020-07-03 17:31:26 +0200

    Add files via upload

---
+++ TechTutor 3.0 v3/JS/desktop.js
@@ -0,0 +1,73 @@
+const slidesContainer = document.querySelectorAll('.desktop-section__img-container');
+const slidesFirst = document.querySelectorAll('.slides-first');
+const slidesSecond = document.querySelectorAll('.slides-second');
+const slidesThird = document.querySelectorAll('.slides-third');
+const arrows = document.querySelectorAll('.arrow');
+const arrowLeft = document.querySelectorAll('.fa-arrow-left');
+const arrowRight = document.querySelectorAll('.fa-arrow-right');
+const btn = document.querySelectorAll('.desktop-section__btn');
+const para = document.querySelectorAll('.desktop-section__para-container');
+const imgPrceCont = document.querySelectorAll('.desktop-section__img-price-container');
+
+const events = ['load', 'resize'];
+for(let event of events){
+    window.addEventListener(event, () => {
+        slidesFirst.forEach((slide, index) => {
+            slide.style.left = `${100 * index}%`;
+        });
+        slidesSecond.forEach((slide, index) => {
+            slide.style.left = `${100 * index}%`;
+        });
+        slidesThird.forEach((slide, index) => {
+            slide.style.left = `${100 * index}%`;
+        });
+    });
+}
+
+function moveToSlide(slidesContainer, currentSlide, targetSlide){
+    slidesContainer.style.transform = `translateX(-${targetSlide.style.left})`;
+    currentSlide.classList.remove('current-slide');
+    targetSlide.classList.add('current-slide');
+}
+
+arrowRight.forEach((arrow, index) => {
+    arrow.addEventListener('click', () => {
+        const currentSlide = slidesContainer[index].querySelectorAll('.current-slide')[0];
+        const nextSlide = currentSlide.nextElementSibling;
+        moveToSlide(slidesContainer[index], currentSlide, nextSlide);
+    });
+});
+
+arrowLeft.forEach((arrow, index) => {
+    arrow.addEventListener('click', () => {
+        const currentSlide = slidesContainer[index].querySelectorAll('.current-slide')[0];
+        const previousSlide = currentSlide.previousElementSibling;
+        moveToSlide(slidesContainer[index], currentSlide, previousSlide);
+    });
+});
+
+btn.forEach((btn, index) => {
+    btn.addEventListener('click', () => {
+        para[index].classList.toggle('para-container--transform');
+        slidesContainer[index].classList.toggle('img-container--transform');
+        arrows[index].classList.toggle('img-container--transform');
+        if(index === 0){
+            slidesFirst.forEach(imgPrice => {
+                imgPrice.classList.toggle('img-container__img--transform');
+            });
+        } else if(index === 1){
+            slidesSecond.forEach(imgPrice => {
+                imgPrice.classList.toggle('img-container__img--transform');
+            });
+        } else{
+            slidesThird.forEach(imgPrice => {
+                imgPrice.classList.toggle('img-container__img--transform');
+            });
+        }
+    });
+
+});
+
+
+
+
---
+++ TechTutor 3.0 v3/JS/scripts.js
@@ -0,0 +1,28 @@
+const burger = document.querySelector('.burger');
+const nav = document.querySelector('nav');
+const pointer = document.querySelector('.pointer');
+const paraContainer = document.querySelectorAll('.para-container--animate');
+
+setInterval(() => pointer.classList.toggle('pointer--animate'), 500);
+
+burger.addEventListener('click', () => {
+    nav.classList.toggle('navbar--animate');
+});
+
+const eventsAnimation = ['scroll', 'load']; // Events is already declared
+for(let event of eventsAnimation){
+    window.addEventListener(event, () => {
+        paraContainer.forEach(tb => {
+            let textPosition = tb.getBoundingClientRect().top;
+            let screenPosition = window.innerHeight;
+            if(textPosition < screenPosition/1.6){
+                tb.classList.add('para-container--animate-toggle');
+                tb.querySelector('p').classList.add('para--animate-toggle');
+            } 
+        });
+    });
+}
+
+$('.pointer').localScroll();
+
+
